e9900000ebce8b445efac8f4fea1f5c68e87ef8b
qunjieye88 <153430431+qunjieye88@users.noreply.github.com>
2024-01-04 17:15:34 +0100

    Update ext.c

--- ext.c
+++ ext.c
@@ -21,7 +21,7 @@ int Borrar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
         char *nombre,  FILE *fich);//OK
 int Copiar(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos,
         EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock,
-        EXT_DATOS *memdatos, char *nombreorigen, char *nombredestino,  FILE *fich);
+        EXT_DATOS *memdatos, char *nombreorigen, char *nombredestino,  FILE *fich);//OK
 void Grabarinodosydirectorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, FILE *fich);
 void GrabarByteMaps(EXT_BYTE_MAPS *ext_bytemaps, FILE *fich);
 void GrabarSuperBloque(EXT_SIMPLE_SUPERBLOCK *ext_superblock, FILE *fich);
@@ -364,10 +364,6 @@ EXT_SIMPLE_SUPERBLOCK *ext_superblock,EXT_DATOS *memdatos, char *nombreorigen, c
     }else{
         printf("EL FICHERO ORIGEN NO EXISTE\n");
     }
-
-}
-
-
 
 }
 
